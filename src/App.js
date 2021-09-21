a316b275b577ffdb5e64f3145b30b49581d88f2b
Govender, Justin <jmg66@students.waikato.ac.nz>
2021-09-21 20:48:35 +1200

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
+import Button from './components/Button';
 import './App.css';
 
 function App() {
+
+  const openInNewTab = (url) => {
+    
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Button text= 'hi' onClick={() => openInNewTab('https://stackoverflow.com')}/>
+      <Button text= 'hi2' onClick={() => openInNewTab('https://google.com')}/>
       </header>
     </div>
   );
